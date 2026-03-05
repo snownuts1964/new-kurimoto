1ee0299ff91878530d3d0ba88925b77f58e73c65
sonwnuts1964 <suzukiseiro@gmail.com>
2026-03-06 02:17:27 +0900

    Initial: Fresh WordPress setup with Docker and npm

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: {
+    main: './wp-content/themes/custom-theme/assets/js/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'wp-content/themes/custom-theme/dist'),
+    filename: 'js/[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+  ],
+  devtool: 'source-map',
+};
